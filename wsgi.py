791495df8514bce3685caf6ccb54b145d4ea132d
Bar Sagiv <barbor161@gmail.com>
2025-08-23 20:23:57 +0300

    modify wsgi

--- wsgi.py
+++ wsgi.py
@@ -2,8 +2,9 @@
 WSGI entry point for Azure App Service
 """
 from app import create_app
-
+import os
 app = create_app()
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port, debug=True)
